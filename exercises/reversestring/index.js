b24be9a32da6da4b2024f147082202865a4d8ebc
Nicholas.Arocho <nick@boldlyfine.com>
2020-11-22 04:26:47 -0800

    finish reverse string algo (3 ways)

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,24 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+
+// solution 1 - probably the best all around
+// function reverse(str) {
+//   return str.split('').reverse().join();
+// }
+
+// solution 2 - without the .reverse() cheater
+function reverse(str) {
+  let reversed = '';
+  for (const character of str) {
+    reversed = character + reversed;
+  }
+  return reversed;
+}
+
+// solution 3 - hot shot 1-liner
+// function reverse(str) {
+//   return str.split('').reduce((reversed, character) => character + reversed, '');
+// }
 
 module.exports = reverse;
